fix numerical issue, and modify the penalty on actions

core/helper_functions.py
@@ -34,14 +34,14 @@ def state_func(configs):
     val_loss_history = configs['val_loss_history']
 
     _inputs = {'inputs':inputs, 'labels':labels}
+
     predicts, _ = student(_inputs, None) # predicts are logits
-    predicts = nn.Softmax()(predicts)
+
+    predicts = nn.LogSoftmax()(predicts)
+    predicts = torch.exp(predicts)
 
     n_samples = inputs.size(0)
     data_features = to_var(torch.zeros(n_samples, num_classes))
-    # import pdb
-    # print ('0000000')
-    # pdb.set_trace()
     data_features[range(n_samples), labels.data] = 1
 
     def sigmoid(x):
@@ -51,22 +51,15 @@ def state_func(configs):
     model_features[:, 0] = current_iter / max_iter # current iteration number
     model_features[:, 1] = 0 if len(train_loss_history) == 0 else sigmoid(sum(train_loss_history)/len(train_loss_history)) # averaged training loss
     model_features[:, 2] = 1 if len(val_loss_history) == 0 else sigmoid(min(val_loss_history))
-    # import pdb
-    # print('11111111')
-    # pdb.set_trace()
+
     combined_features = to_var(torch.zeros(n_samples, 12))
     combined_features[:, :10] = predicts
-    # import pdb
-    # print('22222222')
-    # pdb.set_trace()
-    combined_features[:, 10:11] = -torch.log(predicts[range(n_samples), labels.data])
-    # import pdb
-    # print('33333333')
-    # pdb.set_trace()
+
+    eps = 1e-6
+    combined_features[:, 10:11] = -torch.log(predicts[range(n_samples), labels.data] + eps)
+
     mask = to_var(torch.ones(n_samples, num_classes))
-    # import pdb
-    # print('44444444')
-    # pdb.set_trace()
+
     mask[range(n_samples), labels.data] = 0
     combined_features[:, 11:12] = predicts[range(n_samples), labels.data] - torch.max(mask*predicts, 1)[0]
 

core/teacher_student.py
@@ -142,7 +142,7 @@ class TeacherStudentModel(nn.Module):
                         selected_labels = labels[indices.squeeze()].view(-1, 1)
                         input_pool.append(selected_inputs)
                         label_pool.append(selected_labels)
-                        actions.append(torch.log(predicts.squeeze())*to_var(sampled_actions))
+                        actions.append(torch.log(predicts.squeeze())*to_var(sampled_actions-0.5)*2)
                         if count >= M:
                             break
                     if count >= M:
@@ -185,12 +185,17 @@ class TeacherStudentModel(nn.Module):
                         non_increasing_steps += 1
                     else:
                         non_increasing_steps = 0
-                    loss = -sum([torch.mean(_) for _ in actions])/len(actions)*(reward - baseline)
+
+                    loss = -sum([torch.sum(_) for _ in actions])*(reward - baseline)
                     logger.info('Policy: Iterations [%d], stops at %d/%d to achieve %5.4f, loss: %5.4f, '
                                 'reward: %5.4f(%5.4f)'
                                 %(teacher_updates, i_tau, max_t, acc, loss.cpu().data[0], reward, baseline))
                     rewards.append(reward)
+                    for name, param in teacher.named_parameters():
+                        print (name, param)
                     loss.backward()
+                    for name, param in teacher.named_parameters():
+                        print (name, param.grad)
                     teacher_optimizer.step()
                     teacher_updates += 1
                     teacher_lr_scheduler(teacher_optimizer, teacher_updates)